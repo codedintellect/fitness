5736ca3e3c9f9735cbc573d77e2afc19a8b45f74
Dmitri <codedintellect@gmail.com>
2023-09-02 02:22:03 -0500

    Initial Commit

---
+++ app/page.js
@@ -0,0 +1,9 @@
+export default function Home() {
+  return (
+    <main className="flex justify-center items-center h-screen">
+      <p className="">
+        Initial Commit
+      </p>
+    </main>
+  )
+}
