759f4750c42e6080b8586008f710e069f0c3be32
Dmitri <codedintellect@gmail.com>
2023-09-02 02:56:11 -0500

    Created landing(?) page

--- app/layout.js
+++ app/layout.js
@@ -1,7 +1,7 @@
 import './globals.css'
 import { Inter } from 'next/font/google'
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ['latin', 'cyrillic'] })
 
 export const metadata = {
   title: 'Fitness',
@@ -10,7 +10,7 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en">
+    <html lang="ru">
       <body className={inter.className}>{children}</body>
     </html>
   )
--- app/page.js
+++ app/page.js
@@ -1,8 +1,27 @@
+import Image from 'next/image'
+
 export default function Home() {
   return (
-    <main className="flex justify-center items-center h-screen">
-      <p className="">
-        Initial Commit
+    <main className="flex flex-col justify-items-center basis-full text-left sm:mx-auto sm:max-w-2xl">
+      <Image
+        src=""
+        width={500}
+        height={300}
+        alt="Picture of the author"
+      />
+      <p className="text-4xl font-semibold text-center">
+        Анжела Александра Андерсон
+      </p>
+      <p className="text-2xl text-center">
+        ФИТНЕС-ТРЕНЕР
+      </p>
+      <p className="text-2xl my-4">
+        Личная спортивная практика − более 14 лет.
+        <br></br>
+        Тренерский опыт работы − более 4 лет.
+      </p>
+      <p className="text-2xl font-semibold">
+        Образование:
       </p>
     </main>
   )
