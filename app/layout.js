8de411dd815f23ade530d8db0edbd9f46a78e621
Dmitri <codedintellect@gmail.com>
2023-09-03 01:55:23 -0500

    Font change + Side Menu buttons are now a separate object

--- app/layout.js
+++ app/layout.js
@@ -1,18 +1,18 @@
 'use client'
 
 import './globals.css'
-import { Caveat, Marck_Script } from 'next/font/google'
+import { Neucha, Marck_Script } from 'next/font/google'
 import 'bootstrap-icons/font/bootstrap-icons.css'
 
 import { usePathname } from 'next/navigation';
 import Link from 'next/link'
 
-const caveat = Caveat({ subsets: ['latin', 'cyrillic'], variable: '--caveat-font' })
+const neucha = Neucha({ subsets: ['latin', 'cyrillic'], weight: '400', variable: '--neucha-font' })
 const marck = Marck_Script({ subsets: ['latin', 'cyrillic'], weight: '400', variable: '--marck-font' })
 
 export default function RootLayout({ children }) {
   return (
-    <html lang='ru' className={`${caveat.variable} ${marck.variable}`}>
+    <html lang='ru' className={`${neucha.variable} ${marck.variable}`}>
       <body>
         <SideMenu />
         {children}
@@ -40,30 +40,43 @@ function SideMenu() {
     <main className='fixed'>
       <div className='fixed h-full border-r-2 border-black'>
         <div className='relative h-full flex flex-col gap-2 bg-fallback'>
-          <span className='text-4xl text-center font-bold mx-10'>
+          <span className='text-4xl text-center font-bold mx-10 mt-3'>
             Информация
           </span>
           <div className='grow flex flex-col gap-1 overflow-scroll px-2'>
             {
               options.map((item, index) => {
                 return (
-                  <Link key={index} href={item.path}>
-                    <div className={`w-full text-3xl text-left ${usePathname() == item.path ? "bg-selection" : "bg-primary"} px-3 border-2 border-black rounded-lg`}>
-                      {item.name}
-                    </div>
-                  </Link>
+                  <PageSelector item={item} index={index} />
                 )
               })
             }
+          </div>
+          <div className='flex flex-col gap-1 px-2 text-center'>
+            <PageSelector item={{name: 'Авторизоваться', path: '/auth'}} index={999} />
           </div>
           <span className='text-2xl text-center font-medium'>
             @sanchos.fit
           </span>
         </div>
       </div>
-      <button className='fixed'>
+      <button className='fixed m-2'>
         <i className='bi bi-list text-4xl'></i>
       </button>
     </main>
+  )
+}
+
+function PageSelector({item, index}) {
+  let color = "bg-primary";
+  if (usePathname() == item.path) {
+    color = "bg-selection";
+  }
+  return (
+    <Link key={index} href={item.path}>
+      <div className={`w-full text-3xl ${color} px-3 border-2 border-black rounded-lg`}>
+        {item.name}
+      </div>
+    </Link>
   )
 }
