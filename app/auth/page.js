45b5da3fbbdce3f48d8069582dfbd09d271dbc57
Dmitri <codedintellect@gmail.com>
2023-09-07 01:56:29 -0500

    Styling changes to the auth page

--- app/auth/page.js
+++ app/auth/page.js
@@ -4,17 +4,14 @@ import { login } from '../firebase'
 
 export default function Auth() {
   return (
-    <main className='flex flex-col justify-items-center text-left sm:mx-auto sm:max-w-2xl'>
+    <main className='relative flex flex-col justify-center h-full sm:mx-auto sm:max-w-2xl'>
+      <span className='text-4xl text-center'>
+        Авторизоваться
+      </span>
       <div className='flex flex-col gap-2 mx-6 sm:mx-auto my-6 sm:w-96'>
-        <input id='phonenumber' className='w-full p-2 border-2 border-black rounded-lg' type='tel'>
-        
-        </input>
-        <input id='password' className='w-full p-2 border-2 border-black rounded-lg' type='password'>
-        
-        </input>
-        <button className='w-fit px-4 py-1 mx-auto bg-white border-2 border-black rounded-lg' onClick={submit}>
-          ВОЙТИ
-        </button>
+        <input id='phonenumber' className='w-full text-xl text-center p-1 border-2 border-black rounded-lg' />
+        <input id='password' className='w-full text-xl text-center p-1 border-2 border-black rounded-lg' type='password' />
+        <input className='w-fit text-xl text-center px-4 py-1 mx-auto bg-white border-2 border-black rounded-lg' type='button' value='войти' onClick={submit} />
       </div>
     </main>
   )
