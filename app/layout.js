49329bc6e83a8b366dc7c0875c20f8c3ddf711fc
Dmitri <codedintellect@gmail.com>
2023-09-07 10:10:30 -0500

    Separate "sidemenu" into its own js file

--- app/layout.js
+++ app/layout.js
@@ -12,6 +12,8 @@ import { useState, useEffect } from 'react'
 import { auth } from './firebase'
 import { onAuthStateChanged } from "firebase/auth";
 
+import SideMenu from './components/sidemenu';
+
 const neucha = Neucha({ subsets: ['latin', 'cyrillic'], weight: '400', variable: '--neucha-font' })
 const marck = Marck_Script({ subsets: ['latin', 'cyrillic'], weight: '400', variable: '--marck-font' })
 
@@ -38,81 +40,6 @@ export default function RootLayout({ children }) {
       </body>
     </html>
   )
-}
-
-function SideMenu({sideMenu, toggleMenu, user}) {
-  const options = [
-    {
-      name: 'О тренере Анжеле',
-      path: '/'
-    },
-    {
-      name: 'Запись на тренировки',
-      path: '/schedule'
-    },
-    {
-      name: 'Адрес зала',
-      path: '/location'
-    },
-    {
-      name: 'Цены',
-      path: '/prices'
-    }
-  ]
-  return (
-    <main className='fixed z-[100]'>
-      <div style={{maxWidth: `${sideMenu * 768}px`}} className='fixed h-full border-r-2 border-black overflow-hidden transition-all duration-300'>
-        <div className='relative h-full w-fit flex flex-col gap-2 bg-fallback whitespace-nowrap'>
-          <div className='absolute bottom-0 opacity-50'>
-            <Image
-              src='/sidebg.png'
-              width={829}
-              height={1600}
-              alt='Picture of the author'
-            />
-            <div className='absolute top-0 w-full h-20 bg-gradient-to-b from-fallback to-transparent'></div>
-          </div>
-          <span className='w-full text-4xl text-center font-bold mt-3'>
-            Информация
-          </span>
-          <div className='grow flex flex-col gap-1 overflow-scroll px-2 z-[1]'>
-            {
-              options.map((item, index) => {
-                return (
-                  <PageSelector item={item} key={index} />
-                )
-              })
-            }
-          </div>
-          <div className='flex flex-col gap-1 px-2 text-center z-[1]'>
-            {user==null && <PageSelector item={{name: 'Авторизоваться', path: '/auth'}} key={999} />}
-            {user!=null && <PageSelector item={{name: 'Профиль', path: '/profile'}} key={999} />}
-          </div>
-          <span className='text-2xl text-center font-medium z-[1]'>
-            @sanchos.fit
-          </span>
-        </div>
-      </div>
-      <div className={`fixed inset-0 backdrop-blur-sm bg-black/10 z-[-1] ${sideMenu ? 'opacity-100 visible' : 'opacity-0 invisible'} transition-all duration-300 xl:hidden`}></div>
-      <button className='fixed m-2' onClick={toggleMenu}>
-        <i className='bi bi-list text-4xl'></i>
-      </button>
-    </main>
-  )
-
-  function PageSelector({item}) {
-    let color = "bg-primary";
-    if (usePathname() == item.path) {
-      color = "bg-selection";
-    }
-    return (
-      <Link href={item.path}>
-        <div className={`w-full text-3xl ${color} px-3 border-2 border-black rounded-lg`} onClick={toggleMenu}>
-          {item.name}
-        </div>
-      </Link>
-    )
-  }
 }
 
 function Transition({user}) {
