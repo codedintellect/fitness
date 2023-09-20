3189c40fb28abc400ce17706416944b89a73de7b
Dmitri <codedintellect@gmail.com>
2023-09-20 05:19:41 -0500

    Added admin pages to creators sidemenu This is currently hardcoded to the uid! Instead should check role

--- app/components/sidemenu.js
+++ app/components/sidemenu.js
@@ -29,6 +29,16 @@ export default function SideMenu({sideMenu, toggleMenu}) {
       path: '/questions/'
     }
   ]
+  const adminPages = [
+    {
+      name: 'Тренировки',
+      path: '/scheduling/'
+    },
+    {
+      name: 'Пользователи',
+      path: '/users/'
+    }
+  ]
   return (
     <main className='fixed z-[100]'>
       <div style={{maxWidth: `${sideMenu * 768}px`}} className='fixed h-full border-r-2 border-black overflow-hidden transition-all duration-300'>
@@ -50,6 +60,12 @@ export default function SideMenu({sideMenu, toggleMenu}) {
             }
           </div>
           <div className='flex flex-col gap-1 px-2 text-center z-[1]'>
+            {/* HARD CODED = BAD! */}
+            {(user!=null && user.uid==='1x7zq7YzJlZI6UNLI5hmwJBgKA43') &&
+              adminPages.map((item, index) => (
+                <AdminPage item={item} key={`admin${index}`} />
+              ))
+            }
             {user==null && <PageSelector item={{name: 'Авторизоваться', path: '/auth/'}} key={999} />}
             {user!=null && <PageSelector item={{name: 'Профиль', path: '/profile/'}} key={999} />}
           </div>
@@ -64,6 +80,20 @@ export default function SideMenu({sideMenu, toggleMenu}) {
       </button>
     </main>
   )
+
+  function AdminPage({item}) {
+    let color = "bg-red-300";
+    if (usePathname() == item.path) {
+      color = "bg-red-500";
+    }
+    return (
+      <Link href={item.path}>
+        <div className={`w-full text-3xl ${color} px-3 border-2 border-black rounded-lg`} onClick={toggleMenu}>
+          {item.name}
+        </div>
+      </Link>
+    )
+  }
 
   function PageSelector({item}) {
     let color = "bg-primary";
